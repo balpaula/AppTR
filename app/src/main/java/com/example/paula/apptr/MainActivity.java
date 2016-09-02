d128046dc33eca2f3abc1957e5607e59fb25f388
balpaula <paulabalcells@gmail.com>
2016-09-02 19:21:44 +0200

    commit de tot el projecte

--- app/src/main/java/com/example/paula/apptr/MainActivity.java
+++ app/src/main/java/com/example/paula/apptr/MainActivity.java
@@ -14,14 +14,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void botoElsColorsOnClick(View v){
-        //Canviar de pantalla en clicar el botó, de la pàgina info a Els Colors Invisibles
+        //Canviar de pantalla en clicar el botó, de la pàgina inicial a Els Colors
         Intent passarPantalla = new Intent(this, pantallaElsColors.class);
         startActivity(passarPantalla);
     }
 
+    public void botoComFuncionaOnClick(View v){
+        //Canviar de pantalla en clicar el botó, de la pàgina inicial a Com Funciona?
+        Intent passarPantalla = new Intent(this,pantallaComFunciona3.class);
+        startActivity(passarPantalla);
+    }
+
     public void botoSobreAppOnClick(View v){
-        //Canviar de pantalla en clicar el botó, de la pàgina info a Sobre l'app
+        //Canviar de pantalla en clicar el botó, de la pàgina inicial a Sobre l'app
         Intent passarPantalla = new Intent(this, PantallaSobreApp.class);
+        startActivity(passarPantalla);
+    }
+
+    public void botoFerFoto(View v){
+        //Canviar de pantalla en clicar el botó, de la pàgina inicial a Detecció Colors
+        Intent passarPantalla = new Intent(this, deteccioColors.class);
         startActivity(passarPantalla);
     }
 
